748ac429ad43e462b58f1e5358afffc60678f492
worsleyj <josephworsley23@gmail.com>
2024-02-01 14:54:28 -0500

    added initial factory function

---
+++ script.js
@@ -0,0 +1,15 @@
+const ticTacToe = (function () {
+    function checkWinner(board) {
+        if (true) {
+            return "WIN!"
+        }
+    }
+    return {checkWinner};
+})();
+
+
+console.log(ticTacToe.checkWinner(1))
+
+const player = (function () {
+    
+})
