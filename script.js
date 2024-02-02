a7fa26cde657c46602e31bc4ddaac7c59a652cb9
worsleyj <josephworsley23@gmail.com>
2024-02-02 11:30:49 -0500

    initial factory functions set up

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
+const player1 = createPlayer("player1", "x");
+const player2 = createPlayer("player2", "o");
+
 const ticTacToe = (function () {
+    function beginGame() {
+        
+    }
     function checkWinner(board) {
         if (true) {
             return "WIN!"
@@ -7,9 +13,35 @@ const ticTacToe = (function () {
     return {checkWinner};
 })();
 
+const gameBoard = (function () {
+    let board = [
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0]
+    ]
+    function displayBoard() {
+        console.log("Current Board: ");
+        console.log(board[0]);
+        console.log(board[1]);
+        console.log(board[2]);
+    }
+    function makeMove(column, row, player) {
+        board[column][row] = player.getMark();
+    }
+    return { displayBoard, makeMove };
+})()
 
-console.log(ticTacToe.checkWinner(1))
+function createPlayer (name, mark) {
+    let score = 0;
 
-const player = (function () {
-    
-})
+    const getMark = () => mark;
+    const getScore = () => score;
+    const increaseScore = () => score++;
+
+    return { name, getMark, getScore, increaseScore };
+}
+
+gameBoard.makeMove(1, 2, player1)
+gameBoard.displayBoard();
+player2.increaseScore();
+console.log(player2.name)
