e873579343f00c38e3804c9f098acebf80c56a60
worsleyj <josephworsley23@gmail.com>
2024-02-02 12:11:19 -0500

    checkWinner() implemented and tested, covers columns, rows, and diagonals

--- script.js
+++ script.js
@@ -3,11 +3,22 @@ const player2 = createPlayer("player2", "o");
 
 const ticTacToe = (function () {
     function beginGame() {
-        
+
     }
-    function checkWinner(board) {
-        if (true) {
-            return "WIN!"
+    function checkWinner() {
+        // check rows then columns for winners
+        for (let x = 0; x < 3; x++) {
+            if (gameBoard.board[x][0] == gameBoard.board[x][1] && gameBoard.board[x][1] == gameBoard.board[x][2]) {
+                return x + " Winner! ";
+            } else if (gameBoard.board[0][x] == gameBoard.board[1][x] && gameBoard.board[1][x] == gameBoard.board[2][x]) {
+                return " Winner! " + x;
+            }
+        }
+        // check diagonals
+        if (gameBoard.board[0][0] == gameBoard.board[1][1] && gameBoard.board[1][1] == gameBoard.board[2][2]) {
+            return " Winner! ";
+        } else if (gameBoard.board[0][2] == gameBoard.board[1][1] && gameBoard.board[1][1] == gameBoard.board[2][0]) {
+            return " Winner! ";
         }
     }
     return {checkWinner};
@@ -15,9 +26,9 @@ const ticTacToe = (function () {
 
 const gameBoard = (function () {
     let board = [
-        [0, 0, 0],
-        [0, 0, 0],
-        [0, 0, 0]
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
     ]
     function displayBoard() {
         console.log("Current Board: ");
@@ -28,7 +39,7 @@ const gameBoard = (function () {
     function makeMove(column, row, player) {
         board[column][row] = player.getMark();
     }
-    return { displayBoard, makeMove };
+    return { board, displayBoard, makeMove };
 })()
 
 function createPlayer (name, mark) {
@@ -41,7 +52,10 @@ function createPlayer (name, mark) {
     return { name, getMark, getScore, increaseScore };
 }
 
-gameBoard.makeMove(1, 2, player1)
+gameBoard.makeMove(0, 0, player1)
+gameBoard.makeMove(1, 0, player1)
+gameBoard.makeMove(2, 0, player1)
+console.log(ticTacToe.checkWinner());
 gameBoard.displayBoard();
 player2.increaseScore();
 console.log(player2.name)
