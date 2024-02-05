244475724ec96423f5cf143c2716a483f95c87ae
worsleyj <josephworsley23@gmail.com>
2024-02-05 09:50:46 -0500

    reset functions reintroduced and fixed, accessBoard() function added instead of gameBoard.board

--- script.js
+++ script.js
@@ -2,11 +2,21 @@ const player1 = createPlayer("player1", "x");
 const player2 = createPlayer("player2", "o");
 
 const ticTacToe = (function () {
-    const messages = document.querySelector("#messages")
+    const messages = document.querySelector("#messages");
+    const currentPlayer = document.querySelector("#current-player");
+    
     let turns = 0;
     let winner = 0;
     let currPlayer = player1;
+    displayCurrentPlayer();
 
+    function displayCurrentPlayer() {
+        currentPlayer.textContent = "Current Player: " + currPlayer.name + " - " + currPlayer.getMark(); 
+    }
+    function setCurrentPlayer(player) {
+        currPlayer = player;
+        displayCurrentPlayer();
+    }
     function getCurrentPlayer() {
         return currPlayer;
     }
@@ -17,20 +27,21 @@ const ticTacToe = (function () {
         } else {
             currPlayer = player1;
         }
+        displayCurrentPlayer();
     }
     function checkWinner() {
         // check rows then columns for winners
         for (let x = 0; x < 3; x++) {
-            if (gameBoard.board[x][0] == gameBoard.board[x][1] && gameBoard.board[x][1] == gameBoard.board[x][2] && gameBoard.board[x][0] != "_") {
+            if (gameBoard.accessBoard(x, 0) == gameBoard.accessBoard(x, 1) && gameBoard.accessBoard(x, 1) == gameBoard.accessBoard(x, 2) && gameBoard.accessBoard(x, 0) != "_") {
                 winner = currPlayer.name;
-            } else if (gameBoard.board[0][x] == gameBoard.board[1][x] && gameBoard.board[1][x] == gameBoard.board[2][x] && gameBoard.board[0][x] != "_") {
+            } else if (gameBoard.accessBoard(0, x) == gameBoard.accessBoard(1, x) && gameBoard.accessBoard(1, x) == gameBoard.accessBoard(2, x) && gameBoard.accessBoard(0, x) != "_") {
                 winner = currPlayer.name;
             }
         }
         // check diagonals
-        if (gameBoard.board[0][0] == gameBoard.board[1][1] && gameBoard.board[1][1] == gameBoard.board[2][2] && gameBoard.board[0][0] != "_") {
+        if (gameBoard.accessBoard(0, 0) == gameBoard.accessBoard(1, 1) && gameBoard.accessBoard(1, 1) == gameBoard.accessBoard(2, 2) && gameBoard.accessBoard(0, 0) != "_") {
             winner = currPlayer.name;
-        } else if (gameBoard.board[0][2] == gameBoard.board[1][1] && gameBoard.board[1][1] == gameBoard.board[2][0] && gameBoard.board[2][0] != "_") {
+        } else if (gameBoard.accessBoard(0, 2) == gameBoard.accessBoard(1, 1) && gameBoard.accessBoard(1, 1) == gameBoard.accessBoard(2, 0) && gameBoard.accessBoard(2, 0) != "_") {
             winner = currPlayer.name;
         }
         // if game has run out of empty spaces, declare a tie
@@ -40,12 +51,14 @@ const ticTacToe = (function () {
         }
         // if winner, end the game
         if (winner != 0) {
+            console.log("winner is " + winner)
             messages.style.color = "green";
-            messages.textContent = "You won!";
-            console.log(winner);
+            messages.textContent = winner + " won!";
+            winner = 0;
+            turns = 0;
         }
     }
-    return { turns, messages, getCurrentPlayer, nextPlayer, currPlayer, checkWinner};
+    return { messages, setCurrentPlayer, getCurrentPlayer, nextPlayer, currPlayer, checkWinner};
 })();
 
 const gameBoard = (function () {
@@ -54,6 +67,18 @@ const gameBoard = (function () {
         ["_", "_", "_"],
         ["_", "_", "_"]
     ]
+    function accessBoard(column, row) {
+        // console.log("returning " + board[column][row])
+        return board[column][row];
+    }
+    function resetBoard() {
+        ticTacToe.setCurrentPlayer(player1);
+        board = [
+            ["_", "_", "_"],
+            ["_", "_", "_"],
+            ["_", "_", "_"]
+        ]
+    }
     function displayBoard() {
         console.log("Current Board: ");
         console.log(board[0]);
@@ -81,7 +106,7 @@ const gameBoard = (function () {
             return false;
         }
     }
-    return { board, displayBoard, makeMove };
+    return { board, accessBoard, displayBoard, resetBoard, makeMove };
 })()
 
 const viewController = (function () {
@@ -99,43 +124,60 @@ const viewController = (function () {
 
     oneOne.addEventListener('click', () => {
         gameBoard.makeMove(0, 0, ticTacToe.getCurrentPlayer());
-        oneOne.textContent = gameBoard.board[0][0];
+        oneOne.textContent = gameBoard.accessBoard(0, 0);
     })
     oneTwo.addEventListener('click', () => {
         gameBoard.makeMove(0, 1, ticTacToe.getCurrentPlayer());
-        oneTwo.textContent = gameBoard.board[1][0];
+        oneTwo.textContent = gameBoard.accessBoard(1, 0);
     })
     oneThree.addEventListener('click', () => {
         gameBoard.makeMove(0, 2, ticTacToe.getCurrentPlayer());
-        oneThree.textContent = gameBoard.board[2][0];
+        oneThree.textContent = gameBoard.accessBoard(2, 0);
     })
     twoOne.addEventListener('click', () => {
         gameBoard.makeMove(1, 0, ticTacToe.getCurrentPlayer());
-        twoOne.textContent = gameBoard.board[0][1];
+        twoOne.textContent = gameBoard.accessBoard(0, 1);
     })
     twoTwo.addEventListener('click', () => {
         gameBoard.makeMove(1, 1, ticTacToe.getCurrentPlayer());
-        twoTwo.textContent = gameBoard.board[1][1];
+        twoTwo.textContent = gameBoard.accessBoard(1, 1);
     })
     twoThree.addEventListener('click', () => {
         gameBoard.makeMove(1, 2, ticTacToe.getCurrentPlayer());
-        twoThree.textContent = gameBoard.board[2][1];
+        twoThree.textContent = gameBoard.accessBoard(2, 1);
     })
     threeOne.addEventListener('click', () => {
         gameBoard.makeMove(2, 0, ticTacToe.getCurrentPlayer());
-        threeOne.textContent = gameBoard.board[0][2];
+        threeOne.textContent = gameBoard.accessBoard(0, 2);
     })
     threeTwo.addEventListener('click', () => {
         gameBoard.makeMove(2, 1, ticTacToe.getCurrentPlayer());
-        threeTwo.textContent = gameBoard.board[1][2];
+        threeTwo.textContent = gameBoard.accessBoard(1, 2);
     })
     threeThree.addEventListener('click', () => {
         gameBoard.makeMove(2, 2, ticTacToe.getCurrentPlayer());
-        threeThree.textContent = gameBoard.board[2][2];
+        threeThree.textContent = gameBoard.accessBoard(2, 2);
+        console.log(gameBoard.board[2][2])
     })
     resetBtn.addEventListener('click', () => {
-        location.reload()
+        ticTacToe.messages.style.color = "black";
+        ticTacToe.messages.textContent = "";
+        gameBoard.resetBoard();
+        resetDisplay();
     })
+
+    function resetDisplay() {
+        oneOne.textContent = "_";
+        oneTwo.textContent = "_";
+        oneThree.textContent = "_";
+        twoOne.textContent = "_";
+        twoTwo.textContent = "_";
+        twoThree.textContent = "_";
+        threeOne.textContent = "_";
+        threeTwo.textContent = "_";
+        threeThree.textContent = "_";
+    }
+    return { resetDisplay }
 })();
 
 function createPlayer (name, mark) {
