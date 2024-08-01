b27eb57de4fd6288f7535c8e6c7db0133b2584bb
worsleyj <josephworsley23@gmail.com>
2024-08-01 14:28:21 -0400

    add createPlayer

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 const Gameboard = (function() {
     let board = ["0", "1", "2", "3", "4", "5", "6", "7", "8"];
-    return { board };
+    return {board};
 })();
 
-console.log(Gameboard.board);
+function createPlayer(name, marker) {
+    let score = 0;
+    const addScore = () => score++;
+    const printScore = () => name + "'s score is: " + score;
+    return {name, marker, printScore, addScore}
+}
+
+const playerOne = createPlayer("Player One", "X");
+const playerTwo = createPlayer("Player Two", "O");
+
+playerOne.addScore();
+console.log(playerOne.printScore());
