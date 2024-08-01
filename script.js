ee4710416d14209d485e1a6806977e990cf5c883
worsleyj <josephworsley23@gmail.com>
2024-08-01 14:50:11 -0400

    add player selection, alternating turns

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const Gameboard = (function() {
     let board = ["0", "1", "2", "3", "4", "5", "6", "7", "8"];
-    return {board};
+
+    const displayBoard = () => {
+        console.log("Current Board: ");
+        console.log(board[0], board[1], board[2]);
+        console.log(board[3], board[4], board[5]);
+        console.log(board[6], board[7], board[8]);
+    }
+    return {board, displayBoard};
 })();
 
 function createPlayer(name, marker) {
@@ -13,5 +20,24 @@ function createPlayer(name, marker) {
 const playerOne = createPlayer("Player One", "X");
 const playerTwo = createPlayer("Player Two", "O");
 
-playerOne.addScore();
-console.log(playerOne.printScore());
+const TicTacToe = (function() {
+    let turnNumber = 1;
+    let marker = " ";
+    const nextTurn = () => turnNumber++;
+    const takeTurn = () => {
+        if (turnNumber % 2 != 0) {
+            marker = playerOne.marker;
+        } else {
+            marker = playerTwo.marker;
+        }
+        let playerChoice = prompt("Enter an index to place your marker. Current marker: " + marker)
+        Gameboard.board[playerChoice] = marker;
+        Gameboard.displayBoard();
+        nextTurn();
+    }
+    return {turnNumber, takeTurn}
+})();
+
+TicTacToe.takeTurn();
+TicTacToe.takeTurn();
+TicTacToe.takeTurn();
